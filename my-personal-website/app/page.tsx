9aedefb861785f59c5cc6f8bd5c0d3030c58610e
James Dada <jdada@DTC-61300.local>
2026-01-15 23:25:03 -0500

    feat: initialize Next.js project with TypeScript and Tailwind CSS

---
+++ my-personal-website/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-5xl font-extrabold tracking-tight">
+        Adewale's Personal Website & Engineering Portfolio
+
+        Next, how to add an "about me" section using Tailwind CSS to make it look professional
+      </h1>
+    </main>
+  );
+}
